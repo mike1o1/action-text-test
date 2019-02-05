2217a264fb0e68142f8bfec8294437467d820ad3
Mike Wilson <mike1234@gmail.com>
2019-02-04 16:39:57 -0800

    Add additional scenario and failing tests.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,11 @@
 class Message < ApplicationRecord
     has_rich_text :content
 
+    # also happens with `has_many`
     has_one :post
 
+    has_many :child_posts, class_name: "Post"
+
     accepts_nested_attributes_for :post
+    accepts_nested_attributes_for :child_posts
 end
--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,22 +1,17 @@
 require 'test_helper'
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   test "can update post from within message" do
-    message = Message.create!(title: "Test")
+    message = Message.create!(title: "Seasons")
     post = message.create_post!
-
-    message.reload
 
     # test updating post directly
     message.update!(post_attributes: { content: "Test content", id: post.id })
 
+    # it appears that everything updated?
     assert_equal "Test content", message.post.content.to_plain_text
 
-    # reload and see if things persisted
+    # reload and double check
     message.reload
 
     # this fails. The content is never saved
@@ -24,10 +19,8 @@ class MessageTest < ActiveSupport::TestCase
   end
 
   test "can update post from within a message with another attribute" do
-    message = Message.create!(title: "Test")
+    message = Message.create!(title: "Seasons")
     post = message.create_post!
-
-    message.reload
 
     # test updating post directly
     message.update!(post_attributes: { content: "Test content", id: post.id, title: "New title" })
@@ -38,5 +31,55 @@ class MessageTest < ActiveSupport::TestCase
     message.reload
 
     assert_equal "Test content", message.post.content.to_plain_text
+  end
+
+  test "can update post from within a message and save rich text" do
+    message = Message.create!(title: "Seasons")
+    post = message.create_post!
+
+    message.update!(post_attributes: { content: "Test content", id: post.id })
+
+    assert_equal "Test content", message.post.content.to_plain_text
+
+    # save manually post and reload message
+    message.post.save
+    message.reload
+
+    assert_equal "Test content", message.post.content.to_plain_text
+  end
+
+  test "can update has_many from within a message and save rich text" do
+    message = Message.create!(title: "Seasons")
+    child_post = message.child_posts.create!
+
+    message.update!(child_posts_attributes: { 
+        content: "Test content", 
+        id: child_post.id
+      }
+    )
+
+    assert_equal "Test content", child_post.content.to_plain_text
+
+    child_post.reload
+
+    assert_equal "Test content", child_post.content.to_plain_text
+  end
+
+  test "can update has_many from within a message and save rich text with other attributes" do
+    message = Message.create!(title: "Seasons")
+    child_post = message.child_posts.create!
+
+    message.update!(child_posts_attributes: { 
+        content: "Test content", 
+        id: child_post.id, 
+        title: "Test" 
+      }
+    )
+
+    assert_equal "Test content", child_post.content.to_plain_text
+
+    child_post.reload
+
+    assert_equal "Test content", child_post.content.to_plain_text
   end
 end
